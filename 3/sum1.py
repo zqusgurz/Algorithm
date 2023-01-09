24023b58a7e3334625312231c02093f2008584ae
zqusgurz <rexs333@naver.com>
2023-01-09 19:34:32 +0900

    23/01/09/3

---
+++ 3/sum1.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+suNo, quizNo = map(int, input().split())
+numbers = list(map(int, input().split()))
+prefix_sum = [0]
+temp = 0
+
+for i in numbers:
+    temp = temp + i
+    prefix_sum.append(temp)
+
+for i in range(quizNo):
+    s, e = map(int, input().split())
+    print(prefix_sum[e] - prefix_sum[s-1])
+
+    
